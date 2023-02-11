62738446810642ca5da08da7610c3c0e23d71ca1
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-02-11 16:06:21 -0300

    fix: userId fixo na factory do booking

--- tests/factories/booking-factory.ts
+++ tests/factories/booking-factory.ts
@@ -14,7 +14,7 @@ export async function createBooking(roomId: number, userInput?: User) {
 
   return prisma.booking.create({
     data: {
-      userId: 2,
+      userId: user.id,
       roomId,
     },
   });
